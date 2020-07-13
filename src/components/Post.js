c90e4d7ed0415bc2836f98e846d834a3ce7b64e7
Bobby Gifford <bobby077joe@gmail.com>
2020-07-13 13:51:09 -0700

    Initial commit

---
+++ src/components/Post.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const API_URL = 'http://localhost:1337';
+const formatImageUrl = (url) => `${API_URL}${url}`;
+
+export const Post = ({ post }) => {
+  const url = post?.image[0]?.formats?.medium?.url;
+  const desc = post.description;
+  const likes = post.likes;
+
+  return (
+    <div className='Post'>
+      <img
+        className='Post__Image'
+        src={formatImageUrl(url)}
+        alt='Post thumbnail'
+      />
+      <h4>{desc}</h4>
+      <div>
+        <span>Likes: {likes}</span>
+      </div>
+    </div>
+  );
+};
