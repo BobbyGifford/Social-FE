73bd96520cea2b93f75a9da7d9c326fae0bfcdd8
Bobby Gifford <bobby077joe@gmail.com>
2020-07-13 14:07:52 -0700

    Added mock list with iteration and hooks.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Post } from './components/Post';
 import postData from './data';
 
+const mockPosts = [postData, postData, postData];
+
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    setPosts(mockPosts);
+  }, []);
+
   return (
     <div className='App'>
-      <Post post={postData} />
+      <Post posts={posts} />
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -3,22 +3,28 @@ import React from 'react';
 const API_URL = 'http://localhost:1337';
 const formatImageUrl = (url) => `${API_URL}${url}`;
 
-export const Post = ({ post }) => {
-  const url = post?.image[0]?.formats?.medium?.url;
-  const desc = post.description;
-  const likes = post.likes;
-
+export const Post = ({ posts }) => {
   return (
     <div className='Post'>
-      <img
-        className='Post__Image'
-        src={formatImageUrl(url)}
-        alt='Post thumbnail'
-      />
-      <h4>{desc}</h4>
-      <div>
-        <span>Likes: {likes}</span>
-      </div>
+      {posts.map((post) => {
+        const url = post?.image[0]?.formats?.medium?.url;
+        const desc = post.description;
+        const likes = post.likes;
+        return (
+          <>
+            <img
+              className='Post__Image'
+              src={formatImageUrl(url)}
+              alt='Post thumbnail'
+            />
+            <h4>{desc}</h4>
+            <div>
+              <span>Likes: {likes}</span>
+            </div>
+            <br />
+          </>
+        );
+      })}
     </div>
   );
 };
