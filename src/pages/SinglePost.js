8055d39859590ebaf14c79a8842ce488abee0ce0
Bobby Gifford <bobby077joe@gmail.com>
2020-07-16 15:07:30 -0700

    Added call to SinglePost page.

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Post } from '../Components/Post';
 
-export const SinglePost = () => {
-  return <div>Single Post</div>;
+export const SinglePost = ({ match }) => {
+  // 'http://localhost:1337/posts/1'
+  const { id } = match.params;
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const res = await fetch(`http://localhost:1337/posts/${id}`);
+      const data = await res.json();
+      console.log(data);
+      setPost(data);
+    };
+    fetchPost();
+  }, []);
+
+  // Takes a moment for image to load
+  return <>{post.image ? <Post post={post} /> : <>Loading</>} </>;
 };
