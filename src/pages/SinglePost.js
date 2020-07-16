12e19e4ec5c5784d0ee59b8e5b0403ce2ad7f609
Bobby Gifford <bobby077joe@gmail.com>
2020-07-16 15:47:09 -0700

    Altered loader.

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -5,16 +5,17 @@ export const SinglePost = ({ match }) => {
   // 'http://localhost:1337/posts/1'
   const { id } = match.params;
   const [post, setPost] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPost = async () => {
       const res = await fetch(`http://localhost:1337/posts/${id}`);
       const data = await res.json();
       setPost(data);
+      setLoading(false);
     };
     fetchPost();
   }, [id]);
 
-  // Takes a moment for image to load
-  return <>{post.image ? <Post post={post} /> : <>Loading</>} </>;
+  return <>{loading ? <p>Loading</p> : <Post post={post} />}</>;
 };
